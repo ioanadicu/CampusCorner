b07980fd059589d0710111096ea8241f772d89ce
Laura <laura.gao@student.manchester.ac.uk>
2025-02-25 10:52:40 +0000

    start work on calendarrenderer._calc_rows_needed

--- src/calendar/calendarrenderer.py
+++ src/calendar/calendarrenderer.py
@@ -1,4 +1,8 @@
-from calendarapp import CalendarApp
+import datetime
+import calendarapp
+
+# monday is 0; sunday is 6
+START_OF_WEEK = 0
 
 class CalendarRenderer:
     """
@@ -6,8 +10,16 @@ class CalendarRenderer:
     returns HTML as a string.
     """
 
-    def __init__(self, app: CalendarApp):
+    def __init__(self, app: calendarapp.CalendarApp):
         self.app = app
+
+    def _calc_rows_needed(self) -> int:
+        now = datetime.datetime.now()
+        if self.app.timeframe is calendarapp.Timeframe.MONTH:
+            # calculate rows based on weeks
+            # get weekday the month starts on
+            month_starts_on = datetime.date(now.year, now.month, 1).weekday()
+            # TODO
 
     def render(self):
         return ""  # TODO
