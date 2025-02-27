0597f8ab1468907a35c0e04840ac272752aac172
Laura <laura.gao@student.manchester.ac.uk>
2025-02-27 13:36:56 +0000

    finish CalendarRenderer._calc_rows_needed

--- src/calendar/calendarrenderer.py
+++ src/calendar/calendarrenderer.py
@@ -1,8 +1,12 @@
 import datetime
+import calendar
+
 import calendarapp
+import maths_helpers
 
 # monday is 0; sunday is 6
 START_OF_WEEK = 0
+DAYS_IN_WEEK = 7
 
 class CalendarRenderer:
     """
@@ -18,8 +22,21 @@ class CalendarRenderer:
         if self.app.timeframe is calendarapp.Timeframe.MONTH:
             # calculate rows based on weeks
             # get weekday the month starts on
-            month_starts_on = datetime.date(now.year, now.month, 1).weekday()
-            # TODO
+            monthrange = calendar.monthrange(now.year, now.month)
+            # as calendar.monthrange returns weekday start, length of month
+            # we can do this to calc how many cells are needed
+            no_of_cells = sum(monthrange) - START_OF_WEEK
+            return maths_helpers.ceildiv(no_of_cells, DAYS_IN_WEEK)
+        elif self.app.timeframe is calendarapp.Timeframe.WEEK:
+            # one row per half hour
+            # TODO remove magic numbers
+            return 24 * 2
+        elif self.app.timeframe is calendarapp.Timeframe.DAY:
+            # TODO remove magic number
+            return 24 * 2
+        else:
+            raise ValueError(f"CalendarRenderer.app.timeframe is not MONTH, WEEK, or DAY. Instead it is "
+                             f"{self.app.timeframe}")
 
     def render(self):
         return ""  # TODO
