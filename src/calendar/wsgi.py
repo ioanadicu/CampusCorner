8a3dbe03b67506be48d165e7dfc84f00ce1aea6d
Laura Gao <laura.gao@student.manchester.ac.uk>
2025-03-07 18:40:59 +0000

    add flask calendar tester

--- src/calendar/wsgi.py
+++ src/calendar/wsgi.py
@@ -1,1 +1,24 @@
 from flask import Flask
+import yaml
+
+import calendarapp
+import calendarrenderer
+
+CONFIG_PATH = "cal_testing_config.yaml"
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    # initialise app w/ no calendars yet
+    calendar_app = calendarapp.CalendarApp()
+
+    # add a calendar from url from config file
+    config_file = open(CONFIG_PATH, "r")
+    config = yaml.safe_load(config_file)
+    calendar_app.add_calendar_from_url(config["cal_url"])
+
+    # create renderer
+    renderer = calenderrenderer.CalendarRenderer(calendar_app)
+
+    return renderer.render()
